d814b41a98391dd048ef303c8bd660200311f32d
Per Eckerdal <peck@spotify.com>
2014-11-07 10:24:53 +0100

    Add before and after hooks

--- test/test_suite.js
+++ test/test_suite.js
@@ -2,10 +2,20 @@ it('should be awesome', function() {
 });
 
 describe('Something', function() {
+  before(function beforeHook() {
+    console.log('before');
+  });
+
   it('should work', function() {});
   it('should really work', function() {});
 
   describe('#great', function() {
-    it('should do its thing', function() {});
+    it('should do its thing', function() {
+      console.log('do the thing');
+    });
+
+    after(function afterHook() {
+      console.log('after');
+    });
   });
 });
