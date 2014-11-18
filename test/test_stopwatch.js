437bd248f4f7add9e3f3881b29369f1713ba7a5c
Per Eckerdal <peck@spotify.com>
2014-11-18 09:08:35 +0100

    Rename Timer => Stopwatch
    It's a more precise name for that class

--- test/test_stopwatch.js
+++ test/test_stopwatch.js
@@ -2,64 +2,64 @@
 
 var expect = require('chai').expect;
 var when = require('when');
-var Timer = require('../lib/timer');
+var Stopwatch = require('../lib/stopwatch');
 
 function makeFakeClock() {
   var time = Math.floor(Math.random() * 1000);
-  var timer = function() {
+  var clock = function() {
     return time;
   }
-  timer.step = function(steps) { time += steps; }
-  return timer;
+  clock.step = function(steps) { time += steps; }
+  return clock;
 }
 
-describe('Timer', function() {
+describe('Stopwatch', function() {
   var clock;
-  var timer;
+  var stopwatch;
   beforeEach(function() {
     clock = makeFakeClock();
-    timer = new Timer(clock);
+    stopwatch = new Stopwatch(clock);
   });
 
   it('should report time since start', function() {
     clock.step(100);
-    expect(timer.getTimeSinceStart()).to.be.equal(100);
+    expect(stopwatch.getTimeSinceStart()).to.be.equal(100);
   });
 
   it('should throw exception when asked for time since event that has not happened', function() {
     expect(function() {
-      timer.getTimeSince('nonexistentEvent');
+      stopwatch.getTimeSince('nonexistentEvent');
     }).to.throw(/No registered timestamp for event/);
   });
 
   it('should report time since event', function() {
-    timer.emit('eventName');
+    stopwatch.emit('eventName');
     clock.step(200);
-    expect(timer.getTimeSince('eventName')).to.be.equal(200);
+    expect(stopwatch.getTimeSince('eventName')).to.be.equal(200);
   });
 
   it('should report time since last event', function() {
-    timer.emit('eventName');
+    stopwatch.emit('eventName');
     clock.step(200);
-    timer.emit('eventName');
+    stopwatch.emit('eventName');
     clock.step(200);
-    expect(timer.getTimeSince('eventName')).to.be.equal(200);
+    expect(stopwatch.getTimeSince('eventName')).to.be.equal(200);
   });
 
   it('should report time since last event with same name', function() {
-    timer.emit('eventName1');
+    stopwatch.emit('eventName1');
     clock.step(200);
-    timer.emit('eventName2');
+    stopwatch.emit('eventName2');
     clock.step(200);
-    expect(timer.getTimeSince('eventName1')).to.be.equal(400);
+    expect(stopwatch.getTimeSince('eventName1')).to.be.equal(400);
   });
 
   it('should use wall time as default clock', function() {
-    var timer = new Timer();
+    var stopwatch = new Stopwatch();
     return when()
       .delay(300)
       .then(function() {
-        expect(timer.getTimeSinceStart()).to.be.within(300, 400);
+        expect(stopwatch.getTimeSinceStart()).to.be.within(300, 400);
       });
   });
 });
