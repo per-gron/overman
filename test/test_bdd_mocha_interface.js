191e7275ebe3fb519d15f7968e01eb48239fff60
Per Eckerdal <peck@spotify.com>
2014-11-25 19:09:58 +0100

    Allow setting timeout with this.currentTest.timeout(x)
    For Mocha compatibility

--- test/test_bdd_mocha_interface.js
+++ test/test_bdd_mocha_interface.js
@@ -55,190 +55,207 @@ describe('BDD interface (Mocha flavor)', function() {
     });
   });
 
-  it('should handle describe suites with no body', function() {
-    expect(parseSuite('suite_describe_without_body')).to.be.deep.equal({
-      type: 'suite',
-      contents: [{
+  describe('Suites', function() {
+    it('should handle describe suites with no body', function() {
+      expect(parseSuite('suite_describe_without_body')).to.be.deep.equal({
+        type: 'suite',
+        contents: [{
+          type: 'suite',
+          contents: [],
+          skipped: true,
+          name: 'Skipped suite'
+        }]
+      });
+    });
+
+    it('should handle tests with no body', function() {
+      expect(parseSuite('suite_test_without_body')).to.be.deep.equal({
+        type: 'suite',
+        contents: [{
+          type: 'test',
+          name: 'should be skipped',
+          skipped: true
+        }]
+      });
+    });
+
+    it('should describe suites', function() {
+      expect(parseSuite('suite_empty_describe')).to.be.deep.equal({
+        type: 'suite',
+        contents: [{
+          type: 'suite',
+          contents: [],
+          name: 'Empty'
+        }]
+      });
+    });
+  });
+
+  describe('Tests', function() {
+    it('should declare tests with it', function() {
+      var suite = parseSuite('suite_single_successful_test');
+      expectKeypathIsFunctionAndSetToNull(suite, '.contents[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [{
+          type: 'test',
+          name: 'should succeed',
+          run: null
+        }]
+      });
+    });
+
+    it('should skip tests with it.skip', function() {
+      var suite = parseSuite('suite_single_skipped_test');
+      expectKeypathIsFunctionAndSetToNull(suite, '.contents[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [{
+          type: 'test',
+          name: 'should be skipped',
+          skipped: true,
+          run: null
+        }]
+      });
+    });
+
+    it('should mark only tests with it.only', function() {
+      var suite = parseSuite('suite_single_only_test');
+      expectKeypathIsFunctionAndSetToNull(suite, '.contents[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [{
+          type: 'test',
+          name: 'should be run only',
+          only: true,
+          run: null
+        }]
+      });
+    });
+  });
+
+  describe('Hooks', function() {
+    it('should declare before hooks', function() {
+      var suite = parseSuite('suite_before_hook');
+      expectKeypathIsFunctionAndSetToNull(suite, '.before[0].run');
+      expect(suite).to.be.deep.equal({
         type: 'suite',
         contents: [],
-        skipped: true,
-        name: 'Skipped suite'
-      }]
-    });
-  });
-
-  it('should handle tests with no body', function() {
-    expect(parseSuite('suite_test_without_body')).to.be.deep.equal({
-      type: 'suite',
-      contents: [{
-        type: 'test',
-        name: 'should be skipped',
-        skipped: true
-      }]
-    });
-  });
-
-  it('should describe suites', function() {
-    expect(parseSuite('suite_empty_describe')).to.be.deep.equal({
-      type: 'suite',
-      contents: [{
-        type: 'suite',
-        contents: [],
-        name: 'Empty'
-      }]
-    });
-  });
-
-  it('should declare tests with it', function() {
-    var suite = parseSuite('suite_single_successful_test');
-    expectKeypathIsFunctionAndSetToNull(suite, '.contents[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [{
-        type: 'test',
-        name: 'should succeed',
-        run: null
-      }]
-    });
-  });
-
-  it('should skip tests with it.skip', function() {
-    var suite = parseSuite('suite_single_skipped_test');
-    expectKeypathIsFunctionAndSetToNull(suite, '.contents[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [{
-        type: 'test',
-        name: 'should be skipped',
-        skipped: true,
-        run: null
-      }]
-    });
-  });
-
-  it('should mark only tests with it.only', function() {
-    var suite = parseSuite('suite_single_only_test');
-    expectKeypathIsFunctionAndSetToNull(suite, '.contents[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [{
-        type: 'test',
-        name: 'should be run only',
-        only: true,
-        run: null
-      }]
-    });
-  });
-
-  it('should declare before hooks', function() {
-    var suite = parseSuite('suite_before_hook');
-    expectKeypathIsFunctionAndSetToNull(suite, '.before[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [],
-      before: [{ run: null }]
-    });
-  });
-
-  it('should declare beforeEach hooks', function() {
-    var suite = parseSuite('suite_before_each_hook');
-    expectKeypathIsFunctionAndSetToNull(suite, '.before[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [],
-      before: [{ run: null }]
-    });
-  });
-
-  it('should declare after hooks', function() {
-    var suite = parseSuite('suite_after_hook');
-    expectKeypathIsFunctionAndSetToNull(suite, '.after[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [],
-      after: [{ run: null }]
-    });
-  });
-
-  it('should declare afterEach hooks', function() {
-    var suite = parseSuite('suite_after_each_hook');
-    expectKeypathIsFunctionAndSetToNull(suite, '.after[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [],
-      after: [{ run: null }]
-    });
-  });
-
-  it('should declare hooks with string names', function() {
-    var suite = parseSuite('suite_before_hook_name');
-    expectKeypathIsFunctionAndSetToNull(suite, '.before[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [],
-      before: [{
-        name: 'before hook name',
-        run: null
-      }]
-    });
-  });
-
-  it('should declare hooks with function names', function() {
-    var suite = parseSuite('suite_before_hook_function_name');
-    expectKeypathIsFunctionAndSetToNull(suite, '.before[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [],
-      before: [{
-        name: 'beforeHookName',
-        run: null
-      }]
-    });
-  });
-
-  it('should declare hooks within subsuites', function() {
-    var suite = parseSuite('suite_before_hook_within_describe');
-    expectKeypathIsFunctionAndSetToNull(suite, '.contents[0].before[0].run');
-    expect(suite).to.be.deep.equal({
-      type: 'suite',
-      contents: [{
-        type: 'suite',
-        contents: [],
-        name: 'Suite',
         before: [{ run: null }]
-      }]
+      });
+    });
+
+    it('should declare beforeEach hooks', function() {
+      var suite = parseSuite('suite_before_each_hook');
+      expectKeypathIsFunctionAndSetToNull(suite, '.before[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [],
+        before: [{ run: null }]
+      });
+    });
+
+    it('should declare after hooks', function() {
+      var suite = parseSuite('suite_after_hook');
+      expectKeypathIsFunctionAndSetToNull(suite, '.after[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [],
+        after: [{ run: null }]
+      });
+    });
+
+    it('should declare afterEach hooks', function() {
+      var suite = parseSuite('suite_after_each_hook');
+      expectKeypathIsFunctionAndSetToNull(suite, '.after[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [],
+        after: [{ run: null }]
+      });
+    });
+
+    it('should declare hooks with string names', function() {
+      var suite = parseSuite('suite_before_hook_name');
+      expectKeypathIsFunctionAndSetToNull(suite, '.before[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [],
+        before: [{
+          name: 'before hook name',
+          run: null
+        }]
+      });
+    });
+
+    it('should declare hooks with function names', function() {
+      var suite = parseSuite('suite_before_hook_function_name');
+      expectKeypathIsFunctionAndSetToNull(suite, '.before[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [],
+        before: [{
+          name: 'beforeHookName',
+          run: null
+        }]
+      });
+    });
+
+    it('should declare hooks within subsuites', function() {
+      var suite = parseSuite('suite_before_hook_within_describe');
+      expectKeypathIsFunctionAndSetToNull(suite, '.contents[0].before[0].run');
+      expect(suite).to.be.deep.equal({
+        type: 'suite',
+        contents: [{
+          type: 'suite',
+          contents: [],
+          name: 'Suite',
+          before: [{ run: null }]
+        }]
+      });
     });
   });
 
-  it('should allow getting the timeout for the current test', function() {
-    var suite = parseSuite('suite_timeout_return', { getTimeout: function() {
-      return 12345;
-    }});
-    var fn = getKeypath(suite, '.contents[0].run');
-    expect(fn).to.be.a('function');
-    expect(fn()).to.be.equal(12345);
-  });
+  describe('Timeouts', function() {
+    it('should allow getting the timeout for the current test', function() {
+      var suite = parseSuite('suite_timeout_return', { getTimeout: function() {
+        return 12345;
+      }});
+      var fn = getKeypath(suite, '.contents[0].run');
+      expect(fn).to.be.a('function');
+      expect(fn()).to.be.equal(12345);
+    });
 
-  it('should allow setting the timeout for the current test', function(done) {
-    var suite = parseSuite('suite_timeout_set', { setTimeout: function(value) {
-      expect(value).to.be.equal(10);
-      done();
-    }});
-    var fn = getKeypath(suite, '.contents[0].run');
-    fn();
-  });
+    it('should allow getting the timeout for the current test via the currentTest property', function() {
+      var suite = parseSuite('suite_timeout_return_current_test', { getTimeout: function() {
+        return 12345;
+      }});
+      var fn = getKeypath(suite, '.contents[0].run');
+      expect(fn).to.be.a('function');
+      expect(fn()).to.be.equal(12345);
+    });
 
-  it('should allow getting the timeout in a hook', function() {
-    var suite = parseSuite('suite_timeout_return_in_hook', { getTimeout: function() {
-      return 12345;
-    }});
-    var beforeFn = getKeypath(suite, '.before[0].run');
-    expect(beforeFn).to.be.a('function');
-    beforeFn();
+    it('should allow setting the timeout for the current test', function(done) {
+      var suite = parseSuite('suite_timeout_set', { setTimeout: function(value) {
+        expect(value).to.be.equal(10);
+        done();
+      }});
+      var fn = getKeypath(suite, '.contents[0].run');
+      fn();
+    });
 
-    var fn = getKeypath(suite, '.contents[0].run');
-    expect(fn).to.be.a('function');
-    expect(fn()).to.be.equal(12345);
+    it('should allow getting the timeout in a hook', function() {
+      var suite = parseSuite('suite_timeout_return_in_hook', { getTimeout: function() {
+        return 12345;
+      }});
+      var beforeFn = getKeypath(suite, '.before[0].run');
+      expect(beforeFn).to.be.a('function');
+      beforeFn();
+
+      var fn = getKeypath(suite, '.contents[0].run');
+      expect(fn).to.be.a('function');
+      expect(fn()).to.be.equal(12345);
+    });
   });
 
   it('should declare context global', function() {
