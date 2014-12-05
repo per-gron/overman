5ad2f12a29bb6eef3cbb9d43de352bde6a4860f1
Per Eckerdal <peck@spotify.com>
2014-12-05 09:07:22 +0100

    Unit test soft_kill.js

---
+++ test/test_soft_kill.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2014 Per Eckerdal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var expect = require('chai').expect;
+var softKill = require('../lib/soft_kill');
+var EventEmitter = require('events').EventEmitter;
+
+function fakeProcess(kill) {
+  var process = new EventEmitter();
+  process.kill = kill || function() {};
+  return process;
+}
+
+describe('Soft kill', function() {
+  it('should immediately send a SIGINT to the process', function() {
+    var sigintWasSent = false;
+
+    softKill(fakeProcess(function kill(signal) {
+      sigintWasSent = sigintWasSent || signal === 'SIGINT';
+    }), 0);
+
+    expect(sigintWasSent, 'SIGINT should be sent immediately').to.be.true;
+  });
+
+  it('should set up a timer with the specified timeout', function(done) {
+    function TimeoutTimer(timeout) {
+      expect(timeout).to.be.equal(123);
+      done();
+    };
+    TimeoutTimer.prototype = Object.create(EventEmitter.prototype);
+    softKill(fakeProcess(), 123, TimeoutTimer);
+  });
+
+  it('should cancel the timer when the process exits', function(done) {
+    var process = fakeProcess();
+
+    function TimeoutTimer(timeout) {
+      this.cancel = done;
+    };
+    TimeoutTimer.prototype = Object.create(EventEmitter.prototype);
+    softKill(process, 0, TimeoutTimer);
+
+    process.emit('exit');
+  });
+
+  it('should send a SIGKILL when the timer fires', function(done) {
+    var proc = fakeProcess(function kill(signal) {
+      if (signal === 'SIGKILL') {
+        done();
+      }
+    });
+
+    function TimeoutTimer(timeout) {
+      var self = this;
+      process.nextTick(function() {
+        self.emit('timeout');
+      });
+    }
+    TimeoutTimer.prototype = Object.create(EventEmitter.prototype);
+
+    softKill(proc, 0, TimeoutTimer);
+
+    process.emit('exit');
+  });
+});
