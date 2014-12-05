f5d22340fa5624e4585c00a761983c793aea65a1
Per Eckerdal <peck@spotify.com>
2014-12-05 10:11:27 +0100

    Move tests around a bit
    SIGINT and SIGKILL tests are really timeout things

--- test/test_suite_runner.js
+++ test/test_suite_runner.js
@@ -178,65 +178,6 @@ describe('Suite runner', function() {
     return shouldFail(runTestSuite('suite_single_test_infinite_loop'));
   });
 
-  it('should send SIGINT to tests that time out', function() {
-    var deferred = when.defer();
-
-    function fork() {
-      var child = new EventEmitter();
-      child.stdin = new stream.Readable();
-
-      child.kill = function(signal) {
-        expect(signal).to.be.equal('SIGINT');
-        child.emit('exit', 0, null);
-        child.emit('close');
-        deferred.resolve();
-      };
-
-      return child;
-    }
-
-    return when.all([
-      shouldFail(runTestSuite('suite_single_test_that_never_finishes', [], {
-        childProcess: { fork: fork },
-        timeout: 10
-      }), function(error) {
-        return (error instanceof TestFailureError) && error.message.match(/Tests failed/);
-      }),
-      deferred.promise
-    ]);
-  });
-
-  it('should send SIGKILL to tests that don\'t die after SIGINT', function() {
-    var deferred = when.defer();
-
-    function fork() {
-      var child = new EventEmitter();
-      child.stdin = new stream.Readable();
-
-      child.kill = function(signal) {
-        if (signal === 'SIGINT') {
-          return; // Ignore SIGINT, wait until we get SIGKILL
-        }
-        expect(signal).to.be.equal('SIGKILL');
-        child.emit('exit', 0, null);
-        child.emit('close');
-        deferred.resolve();
-      };
-
-      return child;
-    }
-
-    return when.all([
-      shouldFail(runTestSuite('suite_single_test_that_never_finishes', [], {
-        childProcess: { fork: fork },
-        timeout: 10
-      }), function(error) {
-        return (error instanceof TestFailureError) && error.message.match(/Tests failed/);
-      }),
-      deferred.promise
-    ]);
-  });
-
   it('should run tests sequentially by default', function() {
     var counter = new ParallelismCounter();
     return runTestSuite('suite_various_tests', [counter])
@@ -297,6 +238,65 @@ describe('Suite runner', function() {
       }), function(error) {
         return isTestFailureError(error) && error.message.match(/Timed out while listing tests/);
       });
+    });
+
+    it('should send SIGINT to tests that time out', function() {
+      var deferred = when.defer();
+
+      function fork() {
+        var child = new EventEmitter();
+        child.stdin = new stream.Readable();
+
+        child.kill = function(signal) {
+          expect(signal).to.be.equal('SIGINT');
+          child.emit('exit', 0, null);
+          child.emit('close');
+          deferred.resolve();
+        };
+
+        return child;
+      }
+
+      return when.all([
+        shouldFail(runTestSuite('suite_single_test_that_never_finishes', [], {
+          childProcess: { fork: fork },
+          timeout: 10
+        }), function(error) {
+          return (error instanceof TestFailureError) && error.message.match(/Tests failed/);
+        }),
+        deferred.promise
+      ]);
+    });
+
+    it('should send SIGKILL to tests that don\'t die after SIGINT', function() {
+      var deferred = when.defer();
+
+      function fork() {
+        var child = new EventEmitter();
+        child.stdin = new stream.Readable();
+
+        child.kill = function(signal) {
+          if (signal === 'SIGINT') {
+            return; // Ignore SIGINT, wait until we get SIGKILL
+          }
+          expect(signal).to.be.equal('SIGKILL');
+          child.emit('exit', 0, null);
+          child.emit('close');
+          deferred.resolve();
+        };
+
+        return child;
+      }
+
+      return when.all([
+        shouldFail(runTestSuite('suite_single_test_that_never_finishes', [], {
+          childProcess: { fork: fork },
+          timeout: 10
+        }), function(error) {
+          return (error instanceof TestFailureError) && error.message.match(/Tests failed/);
+        }),
+        deferred.promise
+      ]);
     });
   });
 
