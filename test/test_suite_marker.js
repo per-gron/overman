fbc2eb182e8b09d56a3701b1aa9535adaff74beb
Per Eckerdal <peck@spotify.com>
2014-12-10 09:02:48 +0100

    Fix bug regarding suiteStart in SuiteMarker

--- test/test_suite_marker.js
+++ test/test_suite_marker.js
@@ -130,6 +130,28 @@ describe('SuiteMarker reporter', function() {
         }
       ]);
     });
+
+    it('should emit suiteStart message only for the first test in a suite, even when tests are run in parallel', function() {
+      var path1 = { file: 'file', path: ['test1'] };
+      var path2 = { file: 'file', path: ['test2'] };
+      var suitePath = { file: 'file', path: [] };
+
+      testSuiteMarker([path1, path2], [
+        {
+          emit: { testPath: path1, message: { type: 'start' } },
+          expect: [
+            { testPath: null, message: { type: 'suiteStart', suite: suitePath } },
+            { testPath: path1, message: { type: 'start' } }
+          ]
+        },
+        {
+          emit: { testPath: path2, message: { type: 'start' } },
+          expect: [
+            { testPath: path2, message: { type: 'start' } }
+          ]
+        }
+      ]);
+    });
   });
 
   describe('suiteFinish', function() {
